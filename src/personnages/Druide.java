9cd2c5114ff3a28da06263d3f2a7054511188102
DVL3633A <DVL3633A@u2-209-12.ens.fsi>
2022-09-27 19:53:34 +0200

    V1 de la classe Druide

---
+++ src/personnages/Druide.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package personnages;
+
+/**
+ * @author DVL3633A
+ *
+ */
+public class Druide {
+	private String nom;
+	private int effetPotionMin;
+	private int effetPotionMax;
+	/**
+	 * @param nom
+	 * @param effetPotionMin
+	 * @param effetPotionMax
+	 */
+	public Druide(String nom, int effetPotionMin, int effetPotionMax) {
+		super();
+		this.nom = nom;
+		this.effetPotionMin = effetPotionMin;
+		this.effetPotionMax = effetPotionMax;
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller "
+				+ "d'une force " + effetPotionMin + " à " + effetPotionMax + ".");
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	
+	private void parler(String texte) {
+		System.out.println(prendreParole() + "<< " + texte + ">>");
+		
+	}
+
+	private String prendreParole() {
+		return "Le druide " + nom + " : ";
+	}
+}
