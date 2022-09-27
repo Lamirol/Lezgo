c011eba50a622e5d62f31fc63368e28b2569cce1
DVL3633A <DVL3633A@u2-209-12.ens.fsi>
2022-09-27 19:52:59 +0200

    V1 de la classe Village

---
+++ src/personnages/Village.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package personnages;
+
+/**
+ * @author DVL3633A
+ *
+ */
+public class Village {
+	private String nom;
+
+	/**
+	 * @param nom
+	 */
+	public Village(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	
+	
+}
