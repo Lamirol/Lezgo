3c8aca5e900942f8132b74757f39775d17bad3f5
DVL3633A <DVL3633A@u2-209-12.ens.fsi>
2022-09-27 19:53:20 +0200

    V1 de la classe Gaulois

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package personnages;
+
+/**
+ * @author DVL3633A
+ *
+ */
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+	/**
+	 * @param nom
+	 * @param force
+	 */
+	public Gaulois(String nom, int force) {
+		super();
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<< " + texte + ">>");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+	
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâcoire de " + romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+	
+	public static void main(String[] args) {
+		System.out.println();
+	}
+	
+	
+}
